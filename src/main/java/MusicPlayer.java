ebdffb424c16402c3eb5b089ec2da29b550934de
Rylie Castell <rylie.castell@gmail.com>
2023-08-31 10:33:48 +0200

    implement play method

--- src/main/java/MusicPlayer.java
+++ src/main/java/MusicPlayer.java
@@ -1,6 +1,8 @@
 public class MusicPlayer implements Playable{
+    String song = "Younger Now - Miley Cyrus";
     @Override
     public void play() {
+        System.out.println("Now playing: " + song);
 
     }
 }
