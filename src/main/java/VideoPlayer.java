40519453a5bfe42454ee0e0e2e4ac3926ceb90a0
Rylie Castell <rylie.castell@gmail.com>
2023-08-31 10:38:09 +0200

    implement play method

--- src/main/java/VideoPlayer.java
+++ src/main/java/VideoPlayer.java
@@ -1,6 +1,8 @@
 public class VideoPlayer implements Playable{
+    String video = "Miley Cyrus - Used To Be Young (Official Video)";
     @Override
     public void play() {
+        System.out.println("Now playing: " + video);
 
     }
 }
